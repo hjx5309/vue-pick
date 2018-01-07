bcab401e6935cf0cc7c19a5d2ead725616010aec
hjx <hjx5309@163.com>
2018-01-07 22:48:23 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# hjx
+
+> f
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# 插件plugins里面要做的修改，pick.js 518行要增加输出的对应的id
+self.options.callBack(inputValue,dataCode)
+
+# 插件plugins里面要做的修改，pick.css .ub-f1样式要设置固定高
+.ub-f1 {
+    position: relative;
+    height: 200px;
+}
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="hello">
+   <button @click="pickerSex">选择器</button>
+   <div>{{sex.sex}}</div>
+  </div>
+</template>
+
+<script>
+import Picker from "../plugins/picker/picker.js";
+import  "../plugins/picker/picker.css"
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      sexChose:[{sexId:1,sex:"男"},{sexId:2,sex:"女"}],
+      sex:{sexId:1,sex:"男"}
+    }
+  },
+  methods:{
+    pickerSex(){
+var that=this;
+     new Picker({
+    "title": '请选择', //标题(可选)
+    
+    //默认值： 多个用空格隔开；（可选）
+    "defaultValue": '女', 
+
+    //需要联动级数[1、2、3]（可选）
+    //不传时默认获取数据的深度,最多3级
+    "type": 1,
+
+    //数据(必选)
+    //在使用时，把cityData替换为后台返回的数据即可，但格式要和cityData一致
+    "data": that.sexChose, 
+
+    //数据内的键名称[Code、Name、level](必选)
+    "keys": {
+        "id": "sexId",
+        "value": "sex",
+        "childData": "level"
+    },
+
+   //回调函数（val为当前选中的值）(必选)
+   "callBack": function (val,id) {
+     
+        // 可在此处显示选中的值
+       that.sex.sexId=id;
+        that.sex.sex=val;
+    }
+});
+    }
+  },
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
